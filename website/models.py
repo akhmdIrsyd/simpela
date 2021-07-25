859ce0d88934091ea2cf4644ee90412a7833e359
akhmad Irsyad <blank.irsyad@gmail.com>
2021-07-25 16:17:07 +0700

    repair irigasi

--- website/models.py
+++ website/models.py
@@ -101,7 +101,7 @@ class lahan(models.Model):
     nama_file = models.FileField(upload_to=_upload_path, blank=True, null=True, validators=[FileExtensionValidator(['kmz']), file_size])
 
     def get_upload_path(self, filename):
-        return str("lahan"/+self.id_kab.nama_kab)+"/"+filename
+        return str("lahan/")+str(self.id_kab.nama_kab)+"/"+filename
     def __str__(self):
         return str(self.id_kab)
 
@@ -111,6 +111,6 @@ class irigasi(models.Model):
     nama_file = models.FileField(upload_to=_upload_path, blank=True, null=True, validators=[FileExtensionValidator(['kmz']), file_size])
 
     def get_upload_path(self, filename):
-        return str("irigasi"/+self.id_kab.nama_kab)+"/"+filename
+        return str("irigasi/")+str(self.id_kab.nama_kab)+"/"+filename
     def __str__(self):
         return str(self.id_kab)
