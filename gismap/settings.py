4e54a3932e286339aee865c19518e8ef8f3ec999
Alfrido Rahmat <44556454+AlfridoRahmat@users.noreply.github.com>
2021-07-27 11:39:31 +0800

    ready deploy ?

--- gismap/settings.py
+++ gismap/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 AUTH_USER_MODEL = 'website.User'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','127.0.0.1','localhost','gisinfopspsultra.com','www.gisinfopspsultra.com']
 
 
 # Application definition
